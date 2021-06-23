8312f2bec374965234310f402061763f1a8b7f2a
Phantom 911 <naveedcommand@outlook.com>
2021-06-22 18:01:49 -0700

    added code for one round of fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,47 @@
-// window.alert("This is an alert! Javascript is running!");
+var playername = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun");
+console.log(playername, playerHealth, playerAttack);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+/* function fight() {
+    window.alert("Welcome to Robot Gladiators");
+}
+ */
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playername + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // Check enemy's health
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(enemyName + " attacked " + playername + ". " + playername + " now has " + playerHealth + " health remaining.");
+
+    // Check your health
+    if (playerHealth <=0) {
+        window.alert(playername + " has died!");
+    }
+    else {
+        window.alert(playername + " still has " + playerHealth + " health left.");
+    }
 }
 
-// fight();
-
-var playername = window.prompt("What is your robots name?");
-//window.alert(playername);
-console.log(playername);
-console.log("This logs a string, good for leaving yourself a message");
-console.log (10 + 10);
-console.log("Our Robot's name is: " + playername);
+fight();
