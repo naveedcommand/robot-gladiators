2687ad1f4e45e8ddd103d464c5bc305bece8f175
Phantom 911 <naveedcommand@outlook.com>
2021-06-22 17:09:49 -0700

    added initial lines of JS code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! Javascript is running!");
+// window.alert("This is an alert! Javascript is running!");
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun");
+}
+
+// fight();
+
+var playername = window.prompt("What is your robots name?");
+//window.alert(playername);
+console.log(playername);
+console.log("This logs a string, good for leaving yourself a message");
+console.log (10 + 10);
+console.log("Our Robot's name is: " + playername);
